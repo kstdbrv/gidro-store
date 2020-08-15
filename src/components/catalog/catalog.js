771a24e86d704ca8c1251ae5bf626cb514f2f482
kstdbrv <kstdbrv1@gmail.com>
2020-08-15 23:26:59 +0300

    catalog-page done

--- src/components/catalog/catalog.js
+++ src/components/catalog/catalog.js
@@ -8,6 +8,27 @@ $(document).ready(function () {
     $(this).next().slideToggle(200);
   });
 
+  $('.filter-btn__grid').on('click', function () {
+
+    $(this).addClass('filter-btn__button_active');
+    $('.filter-btn__line').removeClass('filter-btn__button_active');
+    $('.product-item__wrapper').removeClass('product-item__wrapper_list');
+  });
+
+  $('.filter-btn__line').on('click', function () {
+
+    $(this).addClass('filter-btn__button_active');
+    $('.filter-btn__grid').removeClass('filter-btn__button_active');
+    $('.product-item__wrapper').addClass('product-item__wrapper_list');
+  });
+
+  $('.filter-items__btn').on('click', function () {
+
+    $('.filter-items__btn').removeClass('filter-items__btn_active');
+    $(this).addClass('filter-items__btn_active');
+  });
+
+
   $(".js-range-slider").ionRangeSlider({
     type: "double",
   });
